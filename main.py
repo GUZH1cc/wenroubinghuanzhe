af22aeda749a9903232e4b46e27d73b8a139f124
GUZH1cc <111988957+GUZH1cc@users.noreply.github.com>
2023-10-31 19:09:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import os
 
 def get_color():
     # 获取随机颜色
-    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0x000000, 0x000000),0)) #range(n)
+    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0x000000, 0x000000),1)) #range(n)
     color_list = get_colors(100)
     return random.choice(color_list)
 
